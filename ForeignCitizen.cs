6c8850f00413d64ebcfe39e79e2ccfb36203f0f9
NierOx <evgeni10@bk.ru>
2022-03-19 20:28:51 +0300

    Исправление опечатки в описании кода

--- ForeignCitizen.cs
+++ ForeignCitizen.cs
@@ -5,9 +5,14 @@ namespace DemoSplitFiles
 {
     sealed class ForeignCitizen : Citizen
     {
+        //Поля данных класса
+
         private string ForeignPassport;
         private DateTime VisaOpen;
         private DateTime VisaClose;
+
+        //Конструктор Визы и паспорта
+
         public ForeignCitizen(string _name, string _surname, bool _sex,string _password, string _address,string _foreignPassport, DateTime _visaOpen, DateTime _visaClose) : base(_name, _surname, _sex, _password, _address)
         {
             ForeignPassport = _foreignPassport;
@@ -17,6 +22,9 @@ namespace DemoSplitFiles
         public string ForeignPassport {get { return ForeignPassport; } set { ForeignPassport = value; }}
         public DateTime VisaOpen {get { return VisaOpen; } set { VisaOpen = value; } }
         public DateTime VisaClose{ get { return VisaClose; }set { VisaClose = value; }}
+
+        //Вывод данных на консоль
+
         public new void Print()
         {
             base.Print();
