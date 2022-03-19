6640eff71649f35c4177514b116be46317a38d2e
NierOx <evgeni10@bk.ru>
2022-03-19 11:38:34 +0300

    Добавлен файл ForeignCitizen.cs

---
+++ ForeignCitizen.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace DemoSplitFiles
+{
+    sealed class ForeignCitizen : Citizen
+    {
+        private string ForeignPassport;
+        private DateTime VisaOpen;
+        private DateTime VisaClose;
+        public ForeignCitizen(string _name, string _surname, bool _sex,string _password, string _address,string _foreignPassport, DateTime _visaOpen, DateTime _visaClose) : base(_name, _surname, _sex, _password, _address)
+        {
+            ForeignPassport = _foreignPassport;
+            VisaOpen = _visaOpen;
+            VisaClose = _visaClose;
+        }
+        public string ForeignPassport {get { return ForeignPassport; } set { ForeignPassport = value; }}
+        public DateTime VisaOpen {get { return VisaOpen; } set { VisaOpen = value; } }
+        public DateTime VisaClose{ get { return VisaClose; }set { VisaClose = value; }}
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine("foreignPassport: {0}", ForeignPassport);
+            Console.WriteLine("visaOpen: {0}", VisaOpen);
+            Console.WriteLine("visaClose: {0}", VisaClose);
+        }
+    }
+}
